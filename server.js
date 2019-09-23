bfa7c9606cc2067b49a81062829c29ee5f6616d9
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-23 19:54:18 +0000

    / redirects to /posts

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use("/logout", logoutRoutes());
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("/posts");
 });
 
 app.listen(PORT, () => {
