e424cf7155d4b7ffad0f4cc074cdddd667d2ac7a
TheoMLP <theomalaper.cognez@gmail.com>
2019-09-23 19:43:47 +0000

    implementation ejs for the collection page to render the data given by the database

--- routes/collection.js
+++ routes/collection.js
@@ -3,36 +3,64 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/:id', (req, res) => {
-    const queryString = `
-    SELECT *
+    const collectionQuery = `
+    SELECT collections.*, COUNT(DISTINCT posts) as post_count, users.username as owner
     FROM collections
-    WHERE collections.id = $1`
+    LEFT JOIN posts ON collection_id = collections.id
+    JOIN users ON collections.owner_id = users.id
+    WHERE collections.id = $1
+    GROUP BY collections.id, users.username;`
+
+    const postQuery = `
+    SELECT *
+    FROM posts
+    WHERE collection_id = $1;`
+
     const queryParams = [req.params.id]
 
-    db.query(queryString, queryParams)
-       .then(data => {
-         console.log(data);
-       })
-       .catch(err => {
-         console.log(err.stack);
-       })
-  })
-
-  router.get('/api/:username', (req, res) => {
-    const queryString = `
-    SELECT collections.*
-    FROM users
-    JOIN collections ON users.id = owner_id
-    WHERE users.username = $1;`
-    const queryParams = [req.params.username];
-
-    db.query(queryString, queryParams)
-      .then(data => {
-        collections = data.rows
-        res.json(collections);
+    const collection = db.query(collectionQuery, queryParams)
+    const posts = db.query(postQuery, queryParams)
+    Promise.all([collection, posts])
+      .then(values => {
+        let overall = {
+          collection: values[0].rows[0],
+          posts: values[1].rows,
+        }
+        res.render('show_collection', overall)
       })
       .catch(err => {
-        console.log(err.stack);
+         console.log(err.stack);
+      })
+  })
+
+  router.get('/api/:id', (req, res) => {
+    const collectionQuery = `
+    SELECT collections.*, COUNT(DISTINCT posts) as post_count, users.username as owner
+    FROM collections
+    LEFT JOIN posts ON collection_id = collections.id
+    JOIN users ON collections.owner_id = users.id
+    WHERE collections.id = $1
+    GROUP BY collections.id, users.username;`
+
+    const postQuery = `
+    SELECT *
+    FROM posts
+    WHERE collection_id = $1;`
+
+    const queryParams = [req.params.id]
+
+    const collection = db.query(collectionQuery, queryParams)
+    const posts = db.query(postQuery, queryParams)
+    Promise.all([collection, posts])
+      .then(values => {
+        let overall = {
+          collection: values[0].rows[0],
+          posts: values[1].rows,
+        }
+        res.json(overall)
+      })
+      .catch(err => {
+         console.log(err.stack);
       })
   })
   return router
