d56d265c5147cdec16520ca56bd4afca0a32a1f9
tumiduong <duong.julie@live.com>
2019-09-23 20:30:39 +0000

    index ejs

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/posts", postsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("new_post");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
