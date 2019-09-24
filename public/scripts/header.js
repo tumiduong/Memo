f668bfb55aa7cefb85da5c314c1fef9477f43a8d
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-24 14:40:56 +0000

    prevent the default behaviour of submitting the search bar

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,7 +1,10 @@
 $(() => {
-  const search = () => {
-    const action = 'http://localhost:8080/posts' + document.getElementsByName("keyword")[0].value;
-    const form = document.getElementById('formSearch');
-    form.action = action;
-  }
+  $("#search-form").submit(function(e) {
+    e.preventDefault();
+    alert()
+    // location = "http://localhost:8080/posts/search";
+  });
+    // const action = 'http://localhost:8080/posts' + document.getElementsByName("keyword")[0].value;
+    // const form = document.getElementById('formSearch');
+    // form.action = action;
 });
