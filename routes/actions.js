594c1e433f238eee24e4aa9634befa9d2be84758
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-25 18:39:41 +0000

    implement the feature to rate a post, and show a user the rating he gave to a post

--- routes/actions.js
+++ routes/actions.js
@@ -31,5 +31,21 @@ module.exports = (db) => {
         .json({ error: err.message });
     });
   });
+
+  router.post("/rating/:post_id", (req, res) => {
+    const queryString = `INSERT INTO ratings (user_id, post_id, value)
+    VALUES ($1, $2, $3);`;
+    const values = [req.session.id, req.params.post_id, req.body.rating];
+    db.query(queryString, values)
+      .then(() => {
+        res.redirect('/posts');
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
