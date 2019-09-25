088172178632b48af4822e51f6cbcf2263870f66
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-25 15:19:19 +0000

    implement the feature to delete a like on a post

--- routes/actions.js
+++ routes/actions.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.post("/like/:id", (req, res) => {
-    console.log("hi");
+  router.post("/like/:post_id", (req, res) => {
     const queryString = `INSERT INTO likes (user_id, post_id)
     VALUES ($1, $2);`;
-    const values = [req.session.id, req.params.id];
+    const values = [req.session.id, req.params.post_id];
     db.query(queryString, values)
       .then(() => {
         res.redirect('/posts');
@@ -16,6 +15,21 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
+  });
+
+  router.post("/like/:post_id/delete", (req, res) => {
+    const queryString = `DELETE FROM likes
+    WHERE user_id = $1 AND post_id = $2;`;
+    const values = [req.session.id, req.params.post_id];
+    db.query(queryString, values)
+    .then(() => {
+      res.redirect('/posts');
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
   return router;
 };
